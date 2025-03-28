5f59ac42a4097ad6ac229e84a35efd4b4d8398ae
vinithecsar <viniciuscesarbrito@gmail.com>
2025-03-27 21:52:35 -0300

    feat: JoinTable between Category and Product

--- src/main/java/com/vinithecsar/spring_store/entities/Category.java
+++ src/main/java/com/vinithecsar/spring_store/entities/Category.java
@@ -4,10 +4,13 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -26,6 +29,8 @@ public class Category implements Serializable {
   private String name;
 
   @Setter(AccessLevel.NONE)
+  @JsonIgnore
+  @ManyToMany(mappedBy = "categories")
   private Set<Product> products = new HashSet<>();
 
   public Category() {
