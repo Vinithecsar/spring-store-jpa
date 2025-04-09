f43d46719db92c1bff2ce85480ba8c5a2503a50a
vinithecsar <viniciuscesarbrito@gmail.com>
2025-04-08 23:15:49 -0300

    feat: User update

--- src/main/java/com/vinithecsar/spring_store/resources/UserResource.java
+++ src/main/java/com/vinithecsar/spring_store/resources/UserResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -56,6 +57,12 @@ public class UserResource {
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     userService.delete(id);
     return ResponseEntity.noContent().build();
+  }
+
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+    obj = userService.update(id, obj);
+    return ResponseEntity.ok(obj);
   }
 
 }
