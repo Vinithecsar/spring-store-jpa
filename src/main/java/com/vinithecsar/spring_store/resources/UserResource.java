b00f9e5e993de42fd4c1356356bf807bde20042a
vinithecsar <viniciuscesarbrito@gmail.com>
2025-04-08 22:50:33 -0300

    feat: User Insert

--- src/main/java/com/vinithecsar/spring_store/resources/UserResource.java
+++ src/main/java/com/vinithecsar/spring_store/resources/UserResource.java
@@ -1,10 +1,12 @@
 package com.vinithecsar.spring_store.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vinithecsar.spring_store.entities.User;
 import com.vinithecsar.spring_store.exceptions.ResourceNotFoundException;
@@ -12,6 +14,8 @@ import com.vinithecsar.spring_store.services.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -38,6 +42,13 @@ public class UserResource {
     }
 
     return ResponseEntity.ok(obj);
+  }
+
+  @PostMapping
+  public ResponseEntity<User> insert(@RequestBody User obj) {
+    obj = userService.insert(obj);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+    return ResponseEntity.created(uri).body(obj);
   }
 
 }
